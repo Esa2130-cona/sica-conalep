4316bd2360b3e856a0c601e7136625482f2504f6
Esa2130-cona <esaujosafat.clementecruz@gmail.com>
2026-01-15 08:41:58 -0600

    Update control_acceso.py

--- control_acceso.py
+++ control_acceso.py
@@ -106,10 +106,10 @@ if menu == "Puerta de Entrada":
     st.text_input(
         "Esperando lectura...",
         key="scan_input",
-        on_change=procesar_scan
-time.sleep(2)
-st.session_state.resultado = None
-st.rerun()    
+        on_change=procesar_scan 
+        time.sleep(2)
+        st.session_state.resultado = None
+    st.rerun()    
 )
 
 
@@ -215,6 +215,7 @@ elif menu == "Historial Alumnos":
     m = st.text_input("Matrícula").strip()
     if m:
         st.dataframe(df[df["MATRICULA"].astype(str)==m])
+
 
 
 
