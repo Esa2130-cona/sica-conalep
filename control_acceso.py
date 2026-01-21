7a9f117580b0db8a360cf9d044eac7f5ec6665e3
Esa2130-cona <esaujosafat.clementecruz@gmail.com>
2026-01-21 11:19:18 -0600

    Update control_acceso.py

--- control_acceso.py
+++ control_acceso.py
@@ -384,35 +384,31 @@ elif menu == "Credencial Digital":
 
             else:
                 st.success(f"✅ {alumno['nombre']} — GRUPO {alumno['grupo']}")
-            else:
-    st.success(f"¡Hola {alumno['nombre']}! Aquí está tu código de acceso:")
+            # ===== GENERAR QR =====
+                qr = qrcode.QRCode(
+                    version=1,
+                    box_size=8,
+                    border=4
+                )
 
-    qr = qrcode.QRCode(
-        version=1,
-        box_size=4,
-        border=3
-    )
+                qr.add_data(f"{matricula}|{datetime.now(zona).strftime('%Y%m%d')}")
+                qr.make(fit=True)
 
-    qr.add_data(f"{matricula}|{datetime.now(zona).strftime('%Y%m%d')}")
-    qr.make(fit=True)
+                img = qr.make_image(
+                    fill_color="black",
+                    back_color="white"
+                )
 
-    img = qr.make_image(
-        fill_color="black",
-        back_color="white"
-    )
+                buf = BytesIO()
+                img.save(buf, format="PNG")
 
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    buf.seek(0)
+                st.image(
+                    buf,
+                    caption="Código válido SOLO para hoy",
+                    use_container_width=True
+                )
 
-    st.image(
-        buf,
-        width=220,
-        caption="Código válido SOLO para hoy"
-    )
-
-    st.warning("⚠️ Uso indebido de esta credencial será sancionado")
-
+                st.warning("⚠️ Uso indebido de esta credencial será sancionado")
 # ================= MÓDULO: REPORTES =================
 elif menu == "Reportes":
     st.title("🚨 Gestión de Reportes")
@@ -948,6 +944,7 @@ elif menu == "Expediente Digital":
                 st.error("Matrícula no encontrada.")
         except Exception as e:
             st.error(f"Error en el sistema: {e}")
+
 
 
 
